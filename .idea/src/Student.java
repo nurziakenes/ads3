6bd0bd90437cce53bfe1727609b589afbccd5f46
kenesnurziya <nurziakenes@gmail.com>
2026-05-01 14:37:01 +0500

    Created student class

---
+++ .idea/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (age " + age + ")";
+    }
+}
